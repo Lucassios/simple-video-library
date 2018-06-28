cc26fee27cdd8c0148011f0a61d0c17cdd455b31
Lucas Marques <Lucas_Marques@epam.com>
2018-06-28 15:45:49 +0200

    Selecting more videos using ctrl.

--- src/app/components/videos/videos.component.ts
+++ src/app/components/videos/videos.component.ts
@@ -3,7 +3,6 @@ import { Video } from '../../models/video';
 import { VideoLibrary } from '../../models/video-library';
 import { VideoService } from '../../services/video.service';
 import { ElectronService } from '../../providers/electron.service';
-import { VideoLibraryService } from '../../services/video-library.service';
 
 declare var jQuery: any;
 
@@ -16,6 +15,8 @@ export class VideosComponent implements OnInit {
     videos: Video[] = [];
     videosSelected: Video[] = [];
     library: VideoLibrary;
+
+    keyCtrlPressed: boolean = false;
 
     constructor(private videoService: VideoService,
         private electronService: ElectronService) {
@@ -30,20 +31,33 @@ export class VideosComponent implements OnInit {
     }
 
     onDblClickVideo(video) {
-        this.onSelectVideo(null, video);
+        this.clearSelectedVideos();
+        this.selectVideo(video);
         this.electronService.ipcRenderer.send('videos:open', video);
         video.new = false;
         this.videoService.update(video);
     }
 
-    onSelectVideo(event, video: Video) {
-        video.selected = !video.selected;
-        this.clearSelectedVideos();
-        if (video.selected) {
-            this.videosSelected.push(video);
-            this.videoService.setVideoEdition(video);
+    selectVideo(video) {
+        video.selected = true;
+        this.videosSelected.push(video);
+        this.videoService.setVideoEdition(video);
+    }
+
+    unSelectVideo(video) {
+        video.selected = false;
+        this.videoService.setVideoEdition(null);
+    }
+
+    onClickVideo(event, video: Video) {
+        let selected = video.selected;
+        if (!this.keyCtrlPressed) {
+            this.clearSelectedVideos();
+        }
+        if (selected) {
+            this.unSelectVideo(video);
         } else {
-            this.videoService.setVideoEdition(null);
+            this.selectVideo(video);
         }
     }
 
@@ -56,12 +70,19 @@ export class VideosComponent implements OnInit {
         this.videoService.update(video);
     }
 
+    @HostListener('document:keydown', ['$event'])
+    handleKeyupEvent(event: KeyboardEvent) {
+        if(event.key === 'Control') {
+            this.keyCtrlPressed = true;
+        }
+    }
+
     @HostListener('document:keyup', ['$event'])
-    handleDeleteKeyboardEvent(event: KeyboardEvent) {
-        console.log(event.key);
-        if(event.key === 'Delete')
-        {
+    handleKeydownEvent(event: KeyboardEvent) {
+        if(event.key === 'Delete') {
             console.log('delete pressed...');
+        } else if (event.key === 'Control') {
+            this.keyCtrlPressed = false;
         }
     }
 
